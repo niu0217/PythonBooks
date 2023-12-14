bc7c2cdb1369ef07f200e51bb400fdec9e92f8cd
niu0217 <1033856427@qq.com>
2023-12-14 21:37:06 +0800

    完成Automate-Tedious-Tasks:Chapter9-80%

---
+++ Automate-Tedious-Tasks/Dev/Chapter9/test_file_operator.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+# / 运算符的用法
+# homeFolder = Path('/Users/niu0217/niuGithub/PythonBooks')
+# subFolder = Path('FromEntryToPractice')
+#
+# mypath = homeFolder / subFolder
+#
+# print(mypath)
+
+print(Path.home())  # 打印主文件夹
+print(Path.cwd())  # 打印当前路径
+
+
